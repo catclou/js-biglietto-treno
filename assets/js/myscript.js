371d5f8f87dbb3a713cb10b37d34b4e5444ff257
catclou <caterina.moccia@gmail.com>
2022-03-09 15:39:28 +0100

    sketched the logical steps

---
+++ assets/js/myscript.js
@@ -0,0 +1,6 @@
+// 1) prompt per chiedere all'utente i km da percorrere
+// 2) prompt per chiedere l'età del passeggero
+// 3) calcolare i km richiesti moltiplicando per 0.21
+// 4) condizione if per sapere se è minorenne e applicare nel caso true il 20% di sconto
+// 5) condizione if per sapere se è over 65 e applicare nel caso true il 40% di sconto
+// 6) i risultati dovranno avere massimo due decimali (provare ad usare .toFixed(2))
